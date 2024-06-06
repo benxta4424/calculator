aa365b3d9171e40ce4633bf5b3a185d98608b6e8
benxta4424 <podean.beni@gmail.com>
2024-06-06 13:32:12 +0100

    Targetarea butoanelor dinamice cu un event Eventul este declansat in functie de numele butonului Schimbare display din input intr un simplu div Nu stiu la ce m am gandit cu input sincer dar ma bucur ca am reusit sa mi repar greseala Numerele se afiseaza in display insa nu se face nicio operatie momentan

--- java.js
+++ java.js
@@ -1,14 +1,47 @@
-
+    let elementPrim=''
+    let elementSecund=''
+    let operator=''
     let numberOFKeys=1
     const corpCalculator=document.querySelector('.butoane')
 
     const butttons = '789%456*123-0.+='
+    
     
 
     for(numberOFKeys=0;numberOFKeys<16;numberOFKeys++)
         {
             const addKeys=document.createElement('button')
             addKeys.className='butoaneAdaugare'
+            addKeys.name=butttons[numberOFKeys]
             addKeys.textContent=butttons[numberOFKeys]
             corpCalculator.appendChild(addKeys)
         }
+
+    
+    function add(a,b)
+    {
+        return a+b
+    }
+
+    function substract(a,b)
+    {
+        return a-b
+    }
+
+    function multiply(a,b)
+    {
+        return a*b
+    }
+
+    corpCalculator.addEventListener('click',(event)=>
+    {
+
+        const displayAfis=document.querySelector('.display')
+        if(event.target.className==='butoaneAdaugare')
+            {
+                const value=event.target.name
+                elementPrim=value
+                console.log(value)
+            }
+        displayAfis.textContent=elementPrim
+    })
